11627e8b851c8d502f5b967b2197c73e0fe097f1
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-14 14:00:27 -0500

    solved lesson 3

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,10 @@
 # Lesson 3
+tiles = input("Enter the number of tiles: ")
+# input is always a string
+tiles = int(tiles)
+# sqrt is just exponent of a half
+# import math
+# calculation = math.sqrt(tiles)
+calculation = int((tiles ** 0.5) // 1)
+
+print(f"The maximum side length is: {calculation}")
