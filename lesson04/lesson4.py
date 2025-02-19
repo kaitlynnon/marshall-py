b1d14d52f059202a447c0413deefa1d69652857a
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-19 13:59:06 -0500

    solved lesson 4

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,16 @@
 # Lesson 4
+import math
+
+section1 = input("Enter the number of planks as F: ")
+section2 = input("Enter more planks as F: ")
+section3 = input("Enter more planks as F : ")
+
+cans = len(section1) + len(section2) + len(section3)
+boxes = math.ceil(cans / 12)
+leftover = 12*boxes - cans
+
+totalCost = 14.95*boxes
+
+print(f"You need {cans} paint cans")
+print(f"You have {leftover} leftover")
+print(f"The project costs ${totalCost}")
