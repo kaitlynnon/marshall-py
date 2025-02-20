cd07875a7120b58458d126c3f4e350da4743b010
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-20 14:56:07 -0500

    solved lesson 8

--- lesson08/lesson8.py
+++ lesson08/lesson8.py
@@ -1,1 +1,21 @@
 # Lesson 8
+winCounter = 0
+
+for i in range(6):
+    currentResult = input(f"Enter the game {i + 1} result: ")
+
+    if currentResult == "w":
+        winCounter += 1
+
+group = 0
+if winCounter > 4:
+    group = 1
+elif winCounter > 2:
+    group = 2
+elif winCounter > 0:
+    group = 3
+
+if group == 0:
+    print("The player is eliminated")
+else:
+    print(f"The player is placed in {group}")
