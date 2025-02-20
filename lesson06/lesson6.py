f3645301f976ecf58ad512b378f261878ae50c06
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-20 14:42:13 -0500

    lesson 6 completed

--- lesson06/lesson6.py
+++ lesson06/lesson6.py
@@ -1,1 +1,8 @@
 # Lesson 6
+userInput = [int(x) for x in input().split()]
+w1, h1, w2, h2 = userInput
+
+perimeter1 = 2*(w1+w2) + 2*max(h1,h2)
+perimeter2 = 2*max(w1,w2) + 2*(h1+h2)
+
+print(f"Minimum perimeter: {min(perimeter1,perimeter2)}")
