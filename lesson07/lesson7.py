e487f805d3a8b10dc7143a0274fde28638a36b3c
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-19 20:34:41 -0500

    solved lesson 7

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,13 @@
 # Lesson 7
+from random import randrange
+
+dc = int(input("Enter the difficuty class (1-20): "))
+
+roll = randrange(1,21)
+print(f"You have rolled {roll} on the dice")
+
+if roll >= dc:
+    d20 = randrange(1,20)
+    print("You succeeded the ability check")
+else:
+    print("You did not succeed the ability check")
