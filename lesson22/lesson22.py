b3b8076d753d91466bbfd79fa123b2ff044f7856
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-20 22:17:13 -0500

    solved lesson 22

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,13 @@
 # Lesson 22
+
+upperLimit = int(input("Enter N: "))
+fib0 = 0
+fib1 = 1
+fibn = 0
+
+for n in range(2,upperLimit+1):
+    fibn = fib1 + fib0
+    fib0 = fib1
+    fib1 = fibn
+
+print(f"Fibbonaci number {upperLimit} is {fibn}")
