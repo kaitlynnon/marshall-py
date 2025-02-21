0463ab81b0a4ab9a482435448b2ab6608fe7ffd9
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-20 22:07:14 -0500

    solved lesson 20

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,15 @@
 # Lesson 20
+
+totalSum = 0
+
+for num in range(1,10000):
+    factorSum = 0
+    for divider in range(1,num):
+        if num % divider == 0:
+            factorSum += divider
+
+    if factorSum == num:
+        totalSum += num
+        print(f"{num} is a perfect number")
+
+print(f"The total sum for all perfect numbers under 10000 is {totalSum}")
