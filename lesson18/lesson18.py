b94481cec566da6821a12bd24d3b3a3a5fc7f656
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-20 21:48:54 -0500

    solved lesson 18

--- lesson18/lesson18.py
+++ lesson18/lesson18.py
@@ -1,1 +1,9 @@
 # Lesson 18
+
+num = int(input("Enter an integer greater than 0: "))
+
+for divider in range(1,num+1):
+    print(f"Divider variable is {divider}")
+
+    if num % divider == 0:
+        print(f"{divider} is a factor for {num}")
