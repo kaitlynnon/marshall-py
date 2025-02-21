09a9951e6d946d0c0a1dfca36fe29805072860e0
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-20 21:23:56 -0500

    solved lesson 13

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,13 @@
 # Lesson 13
+
+month = int(input("Enter the month (Value from 1-12)"))
+date = int(input("Enter the date (Value from 1-31)"))
+
+if month == 2 and date == 18:
+    print("Special")
+elif month > 2:
+    print("After")
+elif month == 2 and date > 18:
+    print("After")
+elif month <= 2:
+    print("Before")
