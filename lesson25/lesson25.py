6c7bad441fa29b2b2d4e55df6369e43557172fd8
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-20 22:45:39 -0500

    solved lesson 25

--- lesson25/lesson25.py
+++ lesson25/lesson25.py
@@ -1,1 +1,21 @@
 # Lesson 25
+
+num = int(input("Enter a value of N: "))
+numCopy = num
+
+largest = 0
+
+while num % 2 == 0:
+    num //= 2
+    largest = mac(largest,2)
+
+if num != 1:
+    factor = 3
+    while num != 1:
+        if num % factor == 0:
+            largest = max(largest,factor)
+            num //= factor
+        else:
+            factor += 2
+
+print(f"{largest} is the largest prime factor for {numCopy}")
