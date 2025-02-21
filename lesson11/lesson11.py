b9efa7f6faedf27867650c645928eb2e661c1976
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-20 21:02:51 -0500

    solved lesson 11

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,18 @@
 # Lesson 11
+
+point = input()
+point = point.split(' ')
+point = list(map(int,point))
+
+x,y = point
+
+if x > 0:
+    if y > 0:
+        print(f"The point ({x},{y}) is in quadrant 1")
+    else:
+        print(f"The point ({x},{y}) is in quadrant 4")
+else: 
+    if y > 0:
+        print(f"The point ({x},{y}) is in quadrant 2")
+    else:
+        print(f"The point ({x},{y}) is in quadrant 3")
