6721841419fbdc731212188e54a571aff24fbef1
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-20 21:43:32 -0500

    solved lesson 17

--- lesson17/lesson17.py
+++ lesson17/lesson17.py
@@ -1,1 +1,21 @@
 # Lesson 17
+
+num = int(input("Enter thr value of N: "))
+totalProduct = 1
+multiplier = 1
+
+while multiplier <= num:
+    totalProduct = totalProduct*multiplier
+    multiplier = multiplier + 1
+print(f"The factorial of {num} is {totalProduct}")
+
+answer2 = 1
+
+for i in range(num,0,-1):
+    answer2 *= i
+print(f"The factorial of {num} is {answer2}")
+
+answer3 = 1
+for j in range(1,num+1):
+    answer3 *= j
+print(f"The factorial of {num} is {answer3}")
