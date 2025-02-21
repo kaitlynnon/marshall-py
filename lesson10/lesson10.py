204946e6af90db5b2599ca7210b5c98a3cc9b66c
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-20 20:42:32 -0500

    solved lesson 10

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,14 @@
 # Lesson 10
+
+digit = input("Enter the last 4 digits of the number: ")
+
+if digit[0] in {'8','9'}:
+    if digit[-1] in {'8','9'}:
+        if digit[1] == digit[2]:
+            print("This is a telemarketers number")
+        else:
+            print("This is not a telemarketers number")
+    else:
+        print("This is not a telemarketers number")
+else:
+    print("This is not a telemarketers number")
