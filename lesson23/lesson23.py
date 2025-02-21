66e76e101d957297d265f14b27fdab50356e576e
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-02-20 22:33:19 -0500

    solved lesson 23

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,21 @@
 # Lesson 23
+
+loop = True
+
+totalSum = 0
+counter = 0
+
+while loop:
+    userInput = input("Enter the numbers and say Exit when you want to stop: ")
+    if userInput.lower().capitalize() == "Exit":
+        loop = False
+    else:
+        mark = int(userInput)
+        if 0 <= mark <= 100:
+            totalSum += mark
+            counter += 1
+        else:
+            print("invalid input")
+
+average = totalSum/counter
+print(f"The average is {average}")
