f1a9b399d6929324e9af152d475979bbbd9d9db4
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-17 19:55:54 -0400

    solved lesson 27

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,11 @@
 # Lesson 27
+
+def stringCleaner(text):
+    result = ''
+    for character in text:
+        if character.isalpha():
+            result += character.lower()
+    return result
+
+value = stringCleaner("hiii1 hell0o")
+print(f"value is {value}")
