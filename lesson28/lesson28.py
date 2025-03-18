610792a04b5f4a6a4a9fe6208d38b09711df8bc6
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-17 20:07:19 -0400

    solved lesson 28

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,16 @@
 # Lesson 28
+def isPalindrome(text):
+    if not text:
+        return True
+    elif len(text) < 4:
+        return text[0] == text[-1]
+    else:
+        midpoint = len(text) // 2
+        for i in range(0,midpoint):
+            left = text[i]
+            right = text[-1*i - 1]
+
+            if left != right:
+                return False
+        return True
+print(isPalindrome('racecar'))
