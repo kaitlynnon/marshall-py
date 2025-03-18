ee54079d7aeabf91247c978959f6b4c5ffe132b9
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-17 20:23:29 -0400

    solved lesson 31

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,12 @@
 # Lesson 31
+input1 = input("Enter a string: ")
+input2 = input("enter another string: ")
+i = 0
+word1 = sorted(input1)
+word2 = sorted(input2)
+if word1 == word2:
+    print("its an anagram ")
+elif len(word1) != len(word2):
+    print("These are not anagrams")
+else:
+    print("These are not anagrams")
