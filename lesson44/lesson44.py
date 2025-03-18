e5b3a77b6909f93242000ba8bc9299240f3d84fe
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-18 12:05:41 -0400

    solved lesson 44

--- lesson44/lesson44.py
+++ lesson44/lesson44.py
@@ -1,1 +1,15 @@
 # Lesson 44
+def cFrequency(word):
+    cleanWord = sorted(word.lower())
+
+    answer = {}
+
+    for character in cleanWord:
+        if character not in answer:
+            answer[character] = 1
+        else:
+            answer[character] += 1
+    return answer
+
+result = cFrequency("kaitlynnn")
+print(f"Character frequency of kaitlynnn: {result}")
