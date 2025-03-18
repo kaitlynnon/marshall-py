e47db03677c140ec9ed7c93349e09d022a0a8395
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-17 20:15:26 -0400

    solved lesson 29

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,20 @@
 # Lesson 29
+
+def consonantCount(text, vowel = False):
+    counter = 0
+    for character in text:
+        character = character.lower()
+        if character.isalpha() and character in {'a', 'e', 'i', 'o', 'u'}:
+            counter += 1
+    if not vowel:
+        return counter
+    else:
+        counter = 0
+
+        for character in text:
+            character = character.lower()
+            if character.isalpha() and character in {'a', 'e', 'i', 'o', 'u'}:
+                counter += 1
+        return counter
+
+print(f"The number of consonants is {consonantCount('hiiii i like marshmallows')}")
