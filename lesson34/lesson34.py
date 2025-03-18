fc8f48497364a751f7118cfd3b077cbef6c60212
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-17 21:17:16 -0400

    solved lesson 34

--- lesson34/lesson34.py
+++ lesson34/lesson34.py
@@ -1,1 +1,11 @@
 # Lesson 34
+def csvToList(text):
+   
+    csv = text.split(',')
+    aList = []
+
+    for item in csv:
+        aList.append(int(item))
+    return aList
+
+print(csvToList("1,2,3,4,5"))
