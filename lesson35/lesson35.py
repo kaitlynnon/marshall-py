03be09e0add32aca05214c90d47176048a853b81
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-18 10:03:13 -0400

    solved lesson 35

--- lesson35/lesson35.py
+++ lesson35/lesson35.py
@@ -1,1 +1,10 @@
 # Lesson 35
+word = input("Enter a string: ")
+def duplicates(aList):
+    newList = []
+    for item in aList:
+        if item not in newList:
+            newList.append(item)
+    return newList
+
+print(f"The duplicates have been removed: {duplicates(word)}")
