1c5a68b772609531e338b25929134fad66546d87
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-18 10:49:12 -0400

    solved lesson 39

--- lesson39/lesson39.py
+++ lesson39/lesson39.py
@@ -1,1 +1,8 @@
 # Lesson 39
+def gcd(num1,num2):
+    if num2 == 0:
+        return num1
+    else:
+        return gcd(num2, num1 % num2)
+
+print(f"The greatest common divisor between 100 and 289 is {gcd(100,289)}")
