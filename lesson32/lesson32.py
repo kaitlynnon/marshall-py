4bbfec39d46d3643b0838e429d23562da2f6f9cc
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-17 20:45:48 -0400

    solved lesson 32

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,16 @@
 # Lesson 32
+word1 = input("enter a word: ")
+word2 = input("enter another word: ")
+
+def duplicates(word1, word2):
+
+    if not word1 or not word2:
+        return []
+    else:
+        setWord2 = set(word2)
+        result = []
+        for character in word1:
+            if character in setWord2:
+                result.append(character)
+        return sorted(result)
+print(duplicates(word1, word2))
