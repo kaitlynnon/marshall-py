c001518e18fc950228e7875607c0e91aebf2c145
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-18 10:59:52 -0400

    solved lesson 40

--- lesson40/lesson40.py
+++ lesson40/lesson40.py
@@ -1,1 +1,22 @@
 # Lesson 40
+
+def factorsList(num):
+    result = []
+    for divider in range(1,num+1):
+        if num % divider == 0:
+            result.append(divider)
+    return result
+
+def prime(num):
+    return len(factorsList(num)) == 2
+
+def primeList(upperLimit):
+    primes = [2]
+    if upperLimit == 2:
+        return primes
+    else:
+        for num in range(3,upperLimit,2):
+            if prime(num):
+                primes.append(num)
+        return primes
+print(f"All primes under 10: {primeList(10)}")
