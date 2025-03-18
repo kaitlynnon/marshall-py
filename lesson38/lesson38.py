8ca68c6fd0274718d04775d638afbc1393539be2
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-18 10:38:22 -0400

    solved lesson 38

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,15 @@
 # Lesson 38
+
+def palindrome(text):
+    return text == text[::-1]
+
+palidromeNum = []
+
+for num1 in range(999,99,-1):
+    for num2 in range(999,99,-1):
+        product = num1*num2
+
+        if palindrome(str(product)):
+            palidromeNum.append(product)
+
+print(f"The largest palidromic number is {max(palidromeNum)}")
