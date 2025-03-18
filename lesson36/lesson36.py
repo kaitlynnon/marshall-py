c07dd5092c5a5307c6d3744380e838a287295650
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-18 10:10:40 -0400

    solved lesson 36

--- lesson36/lesson36.py
+++ lesson36/lesson36.py
@@ -1,1 +1,14 @@
 # Lesson 36
+numbers = int(input("Enter a number you'd like to check: "))
+
+def factors(num):
+    result = []
+    for divider in range(1,num+1):
+        if num % divider == 0:
+            result.append(divider)
+    return result
+def prime(num):
+    return len(factors(num)) == 2
+
+print(f"The factors are: {factors(numbers)}")
+print(f"Is it prime? {prime(numbers)}")
