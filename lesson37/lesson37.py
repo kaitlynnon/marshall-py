46ea042076303989480549d2a2f73a0a24f76066
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-18 19:41:45 -0400

    solved lesson 37

--- lesson37/lesson37.py
+++ lesson37/lesson37.py
@@ -1,1 +1,27 @@
 # Lesson 37
+
+def strZip(text):
+    result = ''
+    counter = 1
+
+    for i in range(1,len(text)):
+        if text[i] == text[i-1]:
+            counter += 1
+        else:
+            result += text[i-1]
+            result += str(counter)
+            counter = 1
+    result += text[-1] + str(counter)
+
+    if len(result) < len(text):
+        return result
+    else:
+        return text
+
+testy = ["kkkaaaiiiitlynnnn"]
+
+for i, testy in enumerate(testy):
+    output = strZip(testy)
+    print("Test 1: ")
+    print(f"Inputted value: {testy}")
+    print(f"Outputted value: {output}")
