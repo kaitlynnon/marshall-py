d5c764e6bbdedeabda0b740523c87d402550b0b6
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-18 12:09:56 -0400

    solved lesson 45

--- lesson45/lesson45.py
+++ lesson45/lesson45.py
@@ -1,1 +1,15 @@
 # Lesson 45
+
+def wordLength(aList):
+    return {key : len(key) for key in aList}
+print(wordLength(["apple","banana","cherry","date"]))
+
+def dictionaryFib(num):
+    result = {0:0, 1:1}
+    if num in result:
+        return result
+    else:
+        for i in range(2,num+1):
+            result[i] = result[i-1] + result[i-2]
+        return result
+print(dictionaryFib(10))
