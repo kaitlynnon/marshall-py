6772555de70518e2ed478809827cb6c7aa829db9
kaitlynnon <nongrumkaitlyn@gmail.com>
2025-03-18 19:28:20 -0400

    solved lesson 47

--- lesson47/lesson47.py
+++ lesson47/lesson47.py
@@ -1,1 +1,11 @@
 # Lesson 47
+
+def rSum(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return num + rSum(num-1)
+
+print(f"the sum of all numbers from 1-10 is {rSum(10)}")
